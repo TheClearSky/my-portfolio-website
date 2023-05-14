9a42b7b2368cc1fdd157fd84db2fe9713d15496d
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-05-15 04:21:51 +0530

    Added Navbar fully responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Background from './components/Background';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
         <Background>
-        <div style={{zIndex:5,position:'relative',color:'#FFFFFF'}}>Children</div>
+        {/* <div style={{zIndex:5,position:'relative',color:'#FFFFFF'}}>Children</div> */}
+        <Navbar/>
         </Background>
     </>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+    let [openedMenu,setOpenedMenu]=useState(false);
+    function toggleOpenedMenu(){setOpenedMenu((prevOpenedMenu)=>!prevOpenedMenu)};
+  return (
+    <div className={"backgroundblur"+(openedMenu?" openedmenu":"")}>
+      <nav className="navbar">
+          <div className="logo">ClearSky</div>
+
+          <div className="hamburger" onClick={toggleOpenedMenu}>
+              <span className="bar bar1"></span>
+              <span className="bar bar2"></span>
+              <span className="bar bar3"></span>
+              <span className="bar bar4"></span>
+          </div>
+          <div className="navbuttons">
+              <div id="home" className="navbutton  activenavbutton">Home</div>
+              <div id="projects" className="navbutton">Projects</div>
+              <div id="skills" className="navbutton">Skills</div>
+              <div id="contact" className="navbutton">Contact</div>
+              <div id="theme" className="navbutton">Theme</div>
+          </div>
+      </nav>
+    </div>
+  )
+}
