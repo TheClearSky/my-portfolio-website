75f61f988e8e38114a73e8960e644f0ee3085b55
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-05-18 00:20:59 +0530

    Moved RowContainer styles to css and made it responsive on smaller devices

--- frontend/src/components/RowContainer.jsx
+++ frontend/src/components/RowContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './RowContainer.css';
 
-export default function RowContainer({gap:p_gap="3em",width:p_width="100%",maxwidth:p_maxwidth="1200px",padding:p_padding="2em 4em",children:p_children}) {
+export default function RowContainer({children:p_children}) {
   return (
-    <div className="rowcontainer" style={{gap:p_gap,width:p_width,maxWidth:p_maxwidth,padding:p_padding}}>
+    <div className="rowcontainer" >
         {p_children}
     </div>
   )
