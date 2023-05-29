c99f88f7762ddd3be51b88f384424454efa80d30
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-05-29 10:12:22 +0530

    Added minimize on namecard

--- frontend/src/components/NameCard.jsx
+++ frontend/src/components/NameCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./NameCard.css";
 
 export default function NameCard({textDelayInSeconds:p_textDelayInSeconds=5}) {
@@ -10,15 +10,30 @@ export default function NameCard({textDelayInSeconds:p_textDelayInSeconds=5}) {
         (text,index)=>
         <div key={text} style={{animationDelay:`-${(index+1)*p_textDelayInSeconds}s`}}>{text}</div>
         )
+
+    const [minimized,setMinimized]=useState(true);
+    function toggleminimized()
+    {
+        setMinimized((prevMinimized)=>!prevMinimized);
+    }
   return (
-    <div className="glasscard card">
-        <div className="title">Hello, I am Deepak Prasad</div>
-        <div className="content">
-            Let's build something
-        </div>
-        <div className="textanimation" style={{animationDuration:`${p_textDelayInSeconds*textlist.length}s`}}>
-            {animatedtexts}
-        </div>
+    <div className={"glasscard card"+((minimized)?" minimized":"")}>
+        {(minimized)?
+        <>
+            <div onClick={toggleminimized} className="minimizebutton title">+</div>
+            <div className='maximizename title' >NameCard</div>
+        </>
+        :
+        <>
+            <div onClick={toggleminimized} className="minimizebutton title">-</div>
+            <div className="title">Hello, I am Deepak Prasad</div>
+            <div className="content">
+                Let's build something
+            </div>
+            <div className="textanimation" style={{animationDuration:`${p_textDelayInSeconds*textlist.length}s`}}>
+                {animatedtexts}
+            </div>
+        </>}
     </div>
   )
 }
