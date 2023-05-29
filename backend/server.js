94f9f9beb75f47708eb33f26d9db66c64c45fdb6
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-05-30 02:22:36 +0530

    Manual CORS, for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,22 @@ else
 {
     frontendurl=process.env.FRONTEND_URL_DEV;
 }
-app.use(cors({
-    origin:[frontendurl],
-}));
-console.log(frontendurl);
+// app.use(cors({
+//     origin:[frontendurl],
+// }));
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = [frontendurl];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
