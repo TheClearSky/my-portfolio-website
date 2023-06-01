f6c840c4a328dd95d2fb9f826d364c830e1d0f12
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-06-01 18:33:35 +0530

    Added promotionpiecestates and gamemode to slice

--- frontend/src/apiSlices/chessSlice.js
+++ frontend/src/apiSlices/chessSlice.js
@@ -1,9 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+// {{const [singleOrMulti,setSingleOrMulti]=useState("Single Player");
+//     //Pass And Play || Computer
+//     const [singlePlayerMode,setSinglePlayerMode]=useState("Pass And Play");
+//     //Create Game || Join Game || Random Match
+//     const [multiPlayerMode,setMultiPlayerMode]=useState("Join Game");
+//     //Black || White
+//     const [color,setColor]=useState("White");}}
 const initialState = {
     gameIsRunning: false,
     currentlyAnimating: false,
-    signalToStopAnimation:false
+    signalToStopAnimation:false,
+    signalToGetPromotionPiece:false,
+    promotionpiece:null,
+    gamemoMode:{
+        singleOrMulti:"Single Player",
+        singlePlayerMode:"Pass And Play",
+        multiPlayerMode:"Join Game",
+        color:"White"
+    }
 }
 
 export const chessSlice = createSlice({
@@ -24,11 +39,47 @@ export const chessSlice = createSlice({
         },
         sendStopAnimationSignal:(state) =>{
             state.signalToStopAnimation=true;
+        },
+        sendSignalToGetPromotionPiece:(state)=> {
+            state.signalToGetPromotionPiece=true;
+        },
+        setPromotionPiece:(state,action)=>{
+            switch(action.payload.toLowerCase())
+            {
+                case "queen":
+                    state.promotionpiece="q";
+                    break;
+                case "rook":
+                    state.promotionpiece="r";
+                    break;
+                case "knight":
+                    state.promotionpiece="n";
+                    break;
+                case "bishop":
+                    state.promotionpiece="b";
+                    break;
+            }
+            state.signalToGetPromotionPiece=false;
+        },
+        readThePromotionPiece:(state)=>{
+            state.promotionpiece=null;
+        },
+        updateSingleOrMulti:(state,action)=>{
+            state.gamemoMode.singleOrMulti=action.payload;
+        },
+        updateSinglePlayerMode:(state,action)=>{
+            state.gamemoMode.singlePlayerMode=action.payload;
+        },
+        updateMultiPlayerMode:(state,action)=>{
+            state.gamemoMode.multiPlayerMode=action.payload;
+        },
+        updateColor:(state,action)=>{
+            state.gamemoMode.color=action.payload;
         }
     },
 })
 
 
-export const { startGame,endGame,startAnimation,endAnimation,sendStopAnimationSignal } = chessSlice.actions;
+export const { startGame,endGame,startAnimation,endAnimation,sendStopAnimationSignal,sendSignalToGetPromotionPiece,setPromotionPiece,readThePromotionPiece,updateSingleOrMulti,updateSinglePlayerMode,updateMultiPlayerMode,updateColor} = chessSlice.actions;
 
 export default chessSlice.reducer;
