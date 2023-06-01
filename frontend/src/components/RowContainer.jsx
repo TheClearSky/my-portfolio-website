c658bb40386a70bde4ff76238b989fefd77f7d56
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-06-01 18:31:54 +0530

    Row Container now accepts styles

--- frontend/src/components/RowContainer.jsx
+++ frontend/src/components/RowContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './RowContainer.css';
 
-export default function RowContainer({children:p_children}) {
+export default function RowContainer({children:p_children,style:p_style}) {
   return (
-    <div className="rowcontainer" >
+    <div style={p_style} className="rowcontainer" >
         {p_children}
     </div>
   )
