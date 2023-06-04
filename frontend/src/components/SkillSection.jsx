fafe8c95cd9e1c97143261b0b049583a013c8619
ClearSky <42476637+TheClearSky@users.noreply.github.com>
2023-06-04 08:31:00 +0530

    Added icon to skill component

--- frontend/src/components/SkillSection.jsx
+++ frontend/src/components/SkillSection.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import "./SkillSection.css";
+import iconpaths from './IconConfig';
 
-export default function SkillSection({ name: p_name, percentage: p_percentage, animationdelay: p_animationdelay }) {
+export default function SkillSection({ name: p_name, percentage: p_percentage, animationdelay: p_animationdelay,iconname:p_iconname }) {
 
     let barRef = useRef(null);
     let [barMounted,setBarMounted]=useState(false);
@@ -14,7 +15,11 @@ export default function SkillSection({ name: p_name, percentage: p_percentage, a
     return (
         <>
             <div className='skillsection'>
-                <div className='skillsectionname'>{p_name}</div>
+                <div className='skillsectionname'>
+                    {(p_iconname)&&
+                    <img className='skilltechicon' title={p_iconname} alt={p_iconname} key={p_iconname} src={iconpaths[p_iconname]}/>}
+                    <span>{p_name}</span>
+                </div>
                 <div className="skillsectionbarbackground">
                     <div className='skillsectionbar' 
                         style=
